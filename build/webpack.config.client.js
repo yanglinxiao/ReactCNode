326175b065ed3777df5656a43714d57d41d4dd0f
yanglinxiao <511500064@qq.com>
2017-12-21 11:23:07 +0800

    配置webpack-dev-server

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';//判断是否处于开发环境
 const config = {
     entry:{
         app: path.join(__dirname,'../client/app.js')//配置项目入口文件
@@ -29,6 +30,23 @@ const config = {
             template: path.join(__dirname,'../client/template.html')
         })
     ]
+}
+
+//devServer是存在于内存当中的服务器，但是硬盘上存在dist目录会访问硬盘上的，所以启动devServer要先删除硬盘上的dist目录
+if(isDev){
+    config.devServer = {
+        host: '0.0.0.0',//这样配置能让局域网都能访问，如果配置localhost或者127.0.0.1只能是本机访问
+        port: '8888',
+        contentBase: path.join(__dirname,'../dist'),//设置启动webpack服务器的文件夹
+        //hot: true,//启用热加载
+        overlay: {
+            errors: true
+        },//webpack服务器出现错误能够以层叠（弹窗）提醒
+        publicPath: '/public',//配置静态资源路径前缀和output中的一致，否则无法访问
+        historyApiFallback:{
+            index: '/public/index.html'//找不到页面的时候跳转到index.html
+        }
+    }
 }
 
 module.exports = config;
