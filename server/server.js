584a6f6eca46d0756117efe62cff4a95fcd10617
yanglinxiao <511500064@qq.com>
2017-12-29 18:40:57 +0800

    安装开发的插件

--- server/server.js
+++ server/server.js
@@ -8,18 +8,18 @@ const isDev = process.env.NODE_ENV === 'development' ? true : false;
 
 app.use(favicon(path.join(__dirname,'../favicon.ico')));
 
-if(!isDev){
-    app.use('/public',express.static(path.join(__dirname,'../dist')));
-    app.get('*',function (req,res) {
-        const serverEntry = require('../dist/server-entry').default;
-        const appString = reactSSR.renderToString(serverEntry);//把服务器端渲染的入口文件转成HTML模板
-        const indexHtml = fs.readFileSync(path.join(__dirname,'../dist/index.html'),'utf-8');
-        res.send(indexHtml.replace('<!--app-->',appString));
-    })
-}else{
-    const devStatic = require('./util/dev-static');
-    devStatic(app);
-}
+// if(!isDev){
+//     app.use('/public',express.static(path.join(__dirname,'../dist')));
+//     app.get('*',function (req,res) {
+//         const serverEntry = require('../dist/server-entry').default;
+//         const appString = reactSSR.renderToString(serverEntry);//把服务器端渲染的入口文件转成HTML模板
+//         const indexHtml = fs.readFileSync(path.join(__dirname,'../dist/index.html'),'utf-8');
+//         res.send(indexHtml.replace('<!--app-->',appString));
+//     })
+// }else{
+//     const devStatic = require('./util/dev-static');
+//     devStatic(app);
+// }
 
 app.listen(3333,function () {
     console.log('3333 port startup');
